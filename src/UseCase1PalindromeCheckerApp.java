52a9ad9b371cd1285a1b8efecc2fa3c48abbefaa
Sivareddy342 <ss3879@srmist.edu.in>
2026-02-25 14:35:33 +0530

    message

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -1,5 +1,34 @@
 public class UseCase1PalindromeCheckerApp {
-    static void main(String[] args) {
+    static void main(String[] args) { String word = "madam";
+
+        System.out.println("Checking word: " + word);
+
+
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord = reversedWord + word.charAt(i);
+        }
+
+        if (word.equals(reversedWord)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        System.out.println("====================================");
+        System.out.println("Program execution completed.");
+        System.out.println("====================================");
+        String original = "madam";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("The given string \"" + original + "\" is a PALINDROME.");
+        } else {
+            System.out.println("The given string \"" + original + "\" is NOT a palindrome.");
+        }
 
     }
 }
